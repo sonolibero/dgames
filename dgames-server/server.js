1004601150daa8a7eeeed0f96e4b6e2d67f8a5f7
sonolibero.io <ciao@sonolibero.io>
2022-12-22 18:11:37 +0100

    server handle choice

--- dgames-server/server.js
+++ dgames-server/server.js
@@ -5,14 +5,32 @@ const server = http.createServer();
 const ioServer = io(server);
 
 ioServer.on('connection', (socket) => {
-    console.log('A new client has connected!');
-  
-    socket.on('make-choice', (choice) => {
-      console.log(`The client chose ${choice}`);
-      // Handle the choice here
-    });
+  console.log('A new client has connected!');
+
+  socket.on('make-choice', (choice) => {
+    console.log(`The client chose ${choice}`);
+
+    // Process the choice here
+    // For example, you might compare the choice to the server's choice and determine a winner
+    const serverChoice = 'rock';
+    let result;
+    if (choice === serverChoice) {
+      result = 'Tie';
+    } else if (
+      (choice === 'rock' && serverChoice === 'scissors') ||
+      (choice === 'scissors' && serverChoice === 'paper') ||
+      (choice === 'paper' && serverChoice === 'rock')
+    ) {
+      result = 'You win!';
+    } else {
+      result = 'You lose';
+    }
+
+    // Send the result back to the client
+    socket.emit('result', result);
   });
-  
+});
+
 server.listen(3000, () => {
-console.log('Server is listening on port 3000');
+  console.log('Server is listening on port 3000');
 });

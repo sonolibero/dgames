c8cd7ee14fd0d6ecdfa1a1408328f88a5bb30839
sonolibero.io <ciao@sonolibero.io>
2022-12-22 15:58:35 +0100

    connect wallet

--- dgames-client/src/App.js
+++ dgames-client/src/App.js
@@ -1,14 +1,57 @@
 import './App.css';
+import React from 'react';
 import Choices from './Choices';
+// import ethers from 'ethers';
 
 function App() {
+  const [currentAccount, setCurrentAccount] = React.useState("");
+
+  const walletConnected = async () => {
+    const { ethereum } = window;
+
+    if(!ethereum) {
+      return;
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts'});
+
+    if(accounts.length !== 0){
+      setCurrentAccount(accounts[0]);
+    }
+    else {
+      setCurrentAccount("");
+    }
+  }
+
+  const connectWallet = async () => {
+      const { ethereum } = window;
+
+      if(!ethereum) {
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      setCurrentAccount(accounts[0]);
+  }
+
+  const renderConnectWallet = () => (
+    <button onClick={connectWallet}>
+      connect wallet
+    </button>
+  );
+
   const handleChoice = (choice) => {
     // Send the choice to the server or handle it in some other way
   };
 
+  React.useEffect(() => {
+    walletConnected();
+  })
+
   return (
     <div>
-      <Choices handleChoice={handleChoice} />
+      {currentAccount === "" ? ( renderConnectWallet() ) : ( <Choices handleChoice={handleChoice} /> )}
     </div>
   );
 };
--- dgames-client/src/Choices.js
+++ dgames-client/src/Choices.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Choices = ({ handleChoice }) => {
   return (
     <div>
-      <button onClick={() => handleChoice('rock')}>Rock</button>
-      <button onClick={() => handleChoice('paper')}>Paper</button>
-      <button onClick={() => handleChoice('scissors')}>Scissors</button>
+      <button onClick={() => handleChoice('rock')}>rock</button>
+      <button onClick={() => handleChoice('paper')}>paper</button>
+      <button onClick={() => handleChoice('scissors')}>scissors</button>
     </div>
   );
 };

b686c4a38087365b720dcf913037b49707a94545
sonolibero.io <ciao@sonolibero.io>
2022-12-25 10:52:18 +0100

    separate component

--- dgames-client/src/App.js
+++ dgames-client/src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import React from 'react';
 import RockPaperScissor from './RockPaperScissor';
-import socketIOClient from 'socket.io-client';
 // import ethers from 'ethers';
 
 function App() {
   const [currentAccount, setCurrentAccount] = React.useState('');
-  const [choice, setChoice] = React.useState('');
-  const [result, setResult] = React.useState('');
   const [game, setGame] = React.useState('');
-  const socket = socketIOClient('https://dgames-server.sonolibero.repl.co:3000');
 
   const walletConnected = async () => {
     const { ethereum } = window;
@@ -40,19 +36,6 @@ function App() {
       setCurrentAccount(accounts[0]);
   }
 
-  const handleChoice = (c) => {
-    if(choice) {
-      alert('you already chose');
-      return;
-    }
-    setChoice(c);
-
-    socket.emit('make-choice', c);
-    socket.on('result', (r) => {
-      setResult(r);
-    });
-  };
-
   const renderConnectWallet = () => (
     <button onClick={connectWallet}>
       connect wallet
@@ -72,20 +55,6 @@ function App() {
     </div>
   )
 
-  const renderRockPaperScissor = () => (
-    <div>
-      <p>make your choice</p>
-      <RockPaperScissor handleChoice={handleChoice} />
-    </div>
-  )
-
-  const renderPlayerChoice = () => (
-    <div>
-      <div>player choice: {choice}</div>
-      <div>server response: {result}</div>
-    </div>
-  )
-
   React.useEffect(() => {
     walletConnected();
   })
@@ -94,8 +63,7 @@ function App() {
     <div>
       {currentAccount === '' ? ( renderConnectWallet() ) : <p>connected account: {currentAccount}</p>}
       {currentAccount !== '' & !game ? ( renderSelectGame() ) : null}
-      {game === 'rps' ? ( renderRockPaperScissor() ) : null}
-      {choice ? ( renderPlayerChoice() ) : null}
+      {game === 'rps' ? ( <RockPaperScissor /> ) : null}
     </div>
   )
 }
--- dgames-client/src/RockPaperScissor.js
+++ dgames-client/src/RockPaperScissor.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import socketIOClient from 'socket.io-client';
 
-const RockPaperScissor = ({ handleChoice }) => {
+function RockPaperScissor() {
+  const [choice, setChoice] = React.useState('');
+  const [result, setResult] = React.useState('');
+  const socket = socketIOClient('https://dgames-server.sonolibero.repl.co:3000');
+
+  const chooseRock = () => {
+    if(choice) {
+      alert('you already chose');
+      return;
+    }
+    setChoice('rock');
+    sendChoice();
+  }
+
+  const choosePaper = () => {
+    if(choice) {
+      alert('you already chose');
+      return;
+    }
+    setChoice('paper');
+    sendChoice();
+  }
+
+  const chooseScissors = () => {
+    if(choice) {
+      alert('you already chose');
+      return;
+    }
+    setChoice('scissors');
+    sendChoice();
+  }
+
+  const sendChoice = () => {
+    socket.emit('make-choice', choice);
+    socket.on('result', (r) => {
+      setResult(r);
+    });
+  }
+
+  const renderPlayerChoice = () => (
+    <div>
+      <div>player choice: {choice}</div>
+      <div>server response: {result}</div>
+    </div>
+  )
+
   return (
     <div>
-      <button onClick={() => handleChoice('rock')}>rock</button>
-      <button onClick={() => handleChoice('paper')}>paper</button>
-      <button onClick={() => handleChoice('scissors')}>scissors</button>
+      <p>make your choice</p>
+      <button onClick={chooseRock}>rock</button>
+      <button onClick={choosePaper}>paper</button>
+      <button onClick={chooseScissors}>scissors</button>
+      {choice ? ( renderPlayerChoice() ) : null}
     </div>
-  );
-};
+  )
+}
 
 export default RockPaperScissor;
